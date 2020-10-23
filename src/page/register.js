d22f89bb8b44b1d636a07a6726cee627c202e265
tuajie <estuajie@gmail.com>
2020-10-23 14:38:31 +0700

    selesai membuat register ke firebase

--- src/page/register.js
+++ src/page/register.js
@@ -1,22 +1,54 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import firebaseSetting from '../config/firebase'
 
-console.log('config firebase =>',firebaseSetting);
+console.log('config firebase => ',firebaseSetting);
 
-const Register = () =>{
-    return(
-        <Fragment>
-            <div className="dada">
-                <h2>Register Page</h2>
-            </div>
+class Register extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
 
-            <div className="body-register">
-                <input className="form1" type="text" placeholder="email" />
-                <input className="form1" type="password" placeholder="password" />         
-                <button className="button-submit">Register</button>
-            </div>
-        </Fragment>
-    )
+    // fungsi ini digunakan ketika "text area input di ketik", 
+    urusanIsiInputText = (e) => {
+        // console.log(e.target.id)
+        this.setState({
+            [e.target.id]: e.target.value, 
+        })
+    } 
+
+    // fungsi ini digunakan untuk mengeksekusi button ketika di klik
+    urusanButtonKlik = () => {
+        const {email, password} = this.state
+        console.log('data seblum kirim : ', email, password)
+        firebaseSetting.auth().createUserWithEmailAndPassword(email, password)
+            // Handle Suksess here.
+            .then(res => {
+                console.log('success: ', res);
+            })
+            // Handle Errors here.
+            .catch(function(error) { 
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage)
+          });
+    }
+    
+    render(){
+        return(
+            <Fragment>
+                <div className="dada">
+                    <h2>Register Page</h2>
+                </div>
+
+                <div className="body-register">
+                    <input id="email" className="form1" type="text" placeholder="email" onChange={this.urusanIsiInputText} />
+                    <input id="password" className="form1" type="password" placeholder="password" onChange={this.urusanIsiInputText} />         
+                    <button className="button-submit" onClick={this.urusanButtonKlik}>Register</button>
+                </div>
+            </Fragment>
+        )
+    }
 }
-
+ 
 export default Register;
